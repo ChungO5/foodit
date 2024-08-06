58c7cfa5eea0c8f3db647b0827fab1e08917d0bf
KimChungO <kco796@naver.com>
2024-08-06 20:44:37 +0900

    정렬된 데이터 받아오기

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const getFoods = async ({ order }) => {
+export const getFoods = async ({ order = "createdAt" }) => {
     const query = `${order}`;
     const respnse = await fetch(`https://learn.codeit.kr/6666/foods?${query}`);
     const body = await respnse.json();
